3ad8ac51dcc20ba8980525263324402001490f1d
Jakub <jakub.kawka0@gmail.com>
2017-04-29 20:09:29 +0200

    Rozgrzebana funkcja dodawania

--- kalkulatory.py
+++ kalkulatory.py
@@ -1,11 +1,7 @@
-def get_help():
-		print("to jest prosty program kalkulatora. Wprowadz dwie liczby i zatwierdz enter")
+def dodawanie():
+	a = int(input('Podaj pierwszą liczbę'))
+	b = int(input("Podaj pierwszą liczbę"))
+	print(a + b)
 
 
-def dodawanie(a, b):
-	wynik = a + b
-	return wynik
-get_help()
-zm1 = int(input())
-zm2 = int(input())
-print(dodawanie(zm1, zm2))
+dodawanie()
